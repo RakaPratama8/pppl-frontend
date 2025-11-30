1c624bff72d6f019c94c709856882355a71ec762
nxstray <afwanapriansyah@gmail.com>
2025-11-30 20:46:20 +0700

    deleted: unsused class

--- src/app/model/karyawan.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Karyawan {
-  idKaryawan?: number;
-  namaKaryawan: string;
-  emailKaryawan: string;
-  noTelp: string;
-  jabatanPosisi: string;
-  idManager: number;
-  namaManager?: string;
-}
--- src/app/model/manager.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Manager {
-  idManager?: number;
-  namaManager: string;
-  emailManager: string;
-  noTelp: string;
-  divisi: string;
-  tglMulai: Date | string;
-}
