f35e27ffb63d6b72fa60dd9a18192b38f3118caa
nxstray <afwanapriansyah@gmail.com>
2025-12-22 17:32:38 +0700

    fix: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
-import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './guard/auth.guard';
-import { AdminLayoutComponent } from './pages/admin-layout/admin-layout.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LeadScoringComponent } from './pages/lead-scoring/lead-scoring.component';
-import { ClientFormComponent } from './pages/client-form/client-form.component';
+
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/client/login/login.component';
+import { ClientFormComponent } from './pages/client/client-form/client-form.component';
+
+import { KlienComponent } from './pages/admin/klien-page/klien.component';
+import { RekapComponent } from './pages/admin/rekap-page/rekap.component';
+import { ManagerComponent } from './pages/admin/manager-page/manager.component';
+import { LayananComponent } from './pages/admin/layanan-page/layanan.component';
+import { KaryawanComponent } from './pages/admin/karyawan-page/karyawan.component';
+import { DashboardComponent } from './pages/admin/dashboard-page/dashboard.component';
+import { AdminLayoutComponent } from './pages/admin/layout-page/admin-layout.component';
+import { LeadScoringComponent } from './pages/admin/scoring-page/lead-scoring.component';
+import { RequestLayananComponent } from './pages/admin/request-page/request-layanan.component';
 
 export const routes: Routes = [
   {
@@ -14,17 +22,17 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+    loadComponent: () => import('./pages/client/login/login.component').then(m => m.LoginComponent)
   },
     {
     path: 'build',
-    loadComponent: () => import('./pages/client-form/client-form.component').then(m => m.ClientFormComponent)
+    loadComponent: () => import('./pages/client/client-form/client-form.component').then(m => m.ClientFormComponent)
   },
   
   {
     path: 'admin',
     canActivate: [authGuard],
-    loadComponent: () => import('./pages/admin-layout/admin-layout.component')
+    loadComponent: () => import('./pages/admin/layout-page/admin-layout.component')
       .then(m => m.AdminLayoutComponent),
     children: [
       {
@@ -34,13 +42,43 @@ export const routes: Routes = [
       },
       {
         path: 'dashboard',
-        loadComponent: () => import('./pages/dashboard/dashboard.component')
+        loadComponent: () => import('./pages/admin/dashboard-page/dashboard.component')
           .then(m => m.DashboardComponent)
       },
       {
         path: 'lead-scoring',
-        loadComponent: () => import('./pages/lead-scoring/lead-scoring.component')
+        loadComponent: () => import('./pages/admin/scoring-page/lead-scoring.component')
           .then(m => m.LeadScoringComponent)
+      },
+      {
+        path: 'karyawan',
+        loadComponent: () => import('./pages/admin/karyawan-page/karyawan.component')
+          .then(m => m.KaryawanComponent)
+      },
+      {
+        path: 'manager',
+        loadComponent: () => import('./pages/admin/manager-page/manager.component')
+          .then(m => m.ManagerComponent)
+      },
+      {
+        path: 'klien',
+        loadComponent: () => import('./pages/admin/klien-page/klien.component')
+          .then(m => m.KlienComponent)
+      },
+      {
+        path: 'layanan',
+        loadComponent: () => import('./pages/admin/layanan-page/layanan.component')
+          .then(m => m.LayananComponent)
+      },
+      {
+        path: 'request-layanan',
+        loadComponent: () => import('./pages/admin/request-page/request-layanan.component')
+          .then(m => m.RequestLayananComponent)
+      },
+      {
+        path: 'rekap',
+        loadComponent: () => import('./pages/admin/rekap-page/rekap.component')
+          .then(m => m.RekapComponent)
       }
     ]
   },
